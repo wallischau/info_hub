44ac46c2903d36622cf9cdb5d115b22e5cd3109a
wchau <wchau8899@gmail.com>
2017-12-02 00:29:49 -0700

    add heroku setting

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.set('debug', true);
-mongoose.connect("mongodb://localhost/newsscrape", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscrape";
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
